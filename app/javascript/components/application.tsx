01e66ebc16989e3113f7a46959cd4fbe73b45bac
Luis Rodriguez <ljrzarate@gmail.com>
2022-08-26 16:37:49 -0500

    adding post request in the react app

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,12 +1,38 @@
+import axios from "axios";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
 
 interface AppProps {
   arg: string;
 }
 
+interface Post {
+  title: string
+}
+
 const App = ({ arg }: AppProps) => {
-  return <div>{`Hello, ${arg}!`}</div>;
+  const [posts, setPosts] = React.useState([])
+
+  React.useEffect(()=>{
+    axios.get("api/posts.json").then((resp) => {
+      const posts = resp.data
+      setPosts(posts)
+    })
+  }, [])
+
+  const postTitles = posts.map((post)=>{
+    return post["title"]
+  })
+
+
+  return (
+    <>
+      <div>{`Hello, ${arg}!`}</div>
+
+      <div>{`${postTitles}`}</div>
+    </>
+  );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
